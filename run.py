ec31371d4bfb8f0d1cd816a00840fecea52ff2cc
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-06-15 20:50:24 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -53,8 +53,8 @@ def validate_data(values):
 
     return True            
 
-"""
-def update_sales_worksheet(data):
+
+# def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
     """
@@ -63,7 +63,7 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-def update_surplus_worksheet(data):
+# def update_surplus_worksheet(data):
     """
     Update surplus worksheet, add new row with the list data provided
     """
@@ -71,7 +71,7 @@ def update_surplus_worksheet(data):
     surplus_worksheet = SHEET.worksheet("surplus")
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
-"""    
+   
 
 def update_worksheet(data, worksheet):
     """
@@ -103,7 +103,23 @@ def calculate_surplus_data(sales_row):
        surplus = int(stock) - sales
        surplus_data.append(surplus)
 
-    return surplus_data   
+    return surplus_data  
+
+
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data as a list of lists
+    """ 
+    sales = SHEET.worksheet("sales")
+    
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns   
+
 
 def main():
     """
@@ -117,4 +133,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()    
+# main()    
+
+sales_columns = get_last_5_entries_sales()
